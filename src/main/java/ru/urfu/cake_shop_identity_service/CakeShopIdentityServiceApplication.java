eb72a2b25bd92c5ed7914a9f14656f8c317f2ac4
Artamonov Vyacheslav <igrok9636@gmail.com>
2026-02-18 10:54:32 +0500

    init

---
+++ src/main/java/ru/urfu/cake_shop_identity_service/CakeShopIdentityServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.urfu.cake_shop_identity_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakeShopIdentityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakeShopIdentityServiceApplication.class, args);
+	}
+
+}
